ea7e99f0c96608fab4f59b1db3f78e3b533b7dda
Bogdan Bosca <bogdan.bosca@arobs.com>
2019-01-30 13:41:38 +0200

    Added server ip

--- ui/src/config/http/index.js
+++ ui/src/config/http/index.js
@@ -5,7 +5,7 @@ const headers = {
     'Content-Type': 'application/json'
 };
 
-const baseURL = process.env.REACT_APP_MASTER_ADMIN_API || 'http://localhost:9000';
+const baseURL = process.env.REACT_APP_DEKODE_API || 'http://localhost:9000';
 
 const http = Axios.create({
     baseURL,
